54e413ee86dd131380732c9d12cb3efce69675e1
Animesh Shrivatri <animesh.shrivatri2002@gmail.com>
2022-06-17 17:20:38 +0530

    input feild is added and its button also

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import "./App.css";
+import TextField from '@mui/material/TextField';
+// import TextField from "@material-ui/core/TextField";
+import { useState, useEffect } from "react";
+import Button from '@mui/material/Button';
 
 function App() {
+  const [todoInput, setTodoInput] = useState("");
+
+  function addTodo(e) {
+    e.preventDefault();
+    setTodoInput("");
+  }
   return (
     <div className="App">
-      <div>Hello World </div>      
+      <div className="Heading center">Todo list created by Animesh Shrivatri</div>
+      <form className="Form extremCenter">
+        <TextField
+          id="standard-basic"
+          label="Write a Todo"
+          className = "Input"
+          onChange={(e) => setTodoInput(e.target.value)}
+        />
+        <Button 
+          style={{marginLeft:10}} 
+          variant="outlined" 
+          className="InputButton"
+          onClick={addTodo}
+        >Add</Button>
+      </form>
+      
     </div>
   );
 }
